3b864b22e7d67feef924a24821345f681a294baf
Michal Mrnustik <michal.mrnustik@outlook.com>
2025-04-13 16:47:10 +0200

    chore: add test for source generated log entries

--- Extensions.Logging.InMemory.Tests/StructuredLogTests.cs
+++ Extensions.Logging.InMemory.Tests/StructuredLogTests.cs
@@ -150,5 +150,43 @@ public class StructuredLogTests
         }
     }
 
+    [Test]
+    public async Task Log_WithSourceGeneratedLogs_EntryHasLoggedProperties()
+    {
+        // Arrange
+        var logger = new InMemoryLogger<StructuredLogTests>();
+
+        // Act
+        logger.LogSomething("Something", 666);
+
+        // Assert
+        await Assert
+            .That(logger.LoggedEntries)
+            .HasSingleItem();
+        using (Assert.Multiple())
+        {
+            var loggedEntry = logger.LoggedEntries.Single();
+            await Assert.That(loggedEntry.LogLevel)
+                .IsEqualTo(LogLevel.Information);
+            await Assert.That(loggedEntry.EventId)
+                .IsEqualTo(42);
+            await Assert.That(loggedEntry.OriginalFormat)
+                .IsEqualTo("Tested structured log message {StringValue} {IntValue}");
+            await Assert.That(loggedEntry.Properties)
+                .Contains(new KeyValuePair<string, object>("StringValue", "Something"))
+                .And
+                .Contains(new KeyValuePair<string,object>("IntValue", 666));
+        }
+    }
+    
     public record DecomposableObject(string String, int Int);
+}
+
+public static partial class LogExtensions
+{
+    [LoggerMessage(
+        EventId = 42,
+        Level = LogLevel.Information,
+        Message = "Tested structured log message {StringValue} {IntValue}")]
+    public static partial void LogSomething(this ILogger logger, string stringValue, int intValue);
 }
